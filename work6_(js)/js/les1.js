0688d73743b605e69bf210b862e245fc6b983710
Roman Stashchak <stashchak90@gmail.com>
2020-10-29 17:27:19 +0200

    Structure update

---
+++ work6_(js)/js/les1.js
@@ -0,0 +1,117 @@
+//document.write("Hello world!");
+//alert("Hello world");
+//a=confirm ("How old are you?")
+//console.log (a);
+
+/*1. task
+
+-
+- 
+- при помощи 3х разных prompt() получить 3 слова которые являются вашими ФИО. Для фамилии имени и отчества создать разные переменные. Вывести каждую при помощи console.log , alert, document.write
+- Взять переменные из задания 4 и сконкатенировать их в одной переменной person.
+- Взять задние 4 и 5 и применить его к ФИО всех членов своей семьи.
+- при помощи prompt() получить 3 числа. Привести их к числовому типу при помощи +. вывести их в консоль.
+- при помощи prompt() получить 4 числа. Привести их к числовому типу при помощи parseInt. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+- при помощи prompt()  получить 3 числа с плавающей точекой. при помощи parseFloat привести их к соответсвующему типу. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+- при помощи prompt()  получить 3 числа с плавающей точекой. Округлить их при помощи Math.round Сложить их между собой записав результат в переменную  и вывести в консоль браузера
+- при помощи prompt()  получить 2 целых числа. Привести их к целочисленному типу. Первое число - это число которое будут возводить в степень. Второе число - это число которое является степенью. При помощи Math.pow возвести первое число в степень второго числа.
+- При помощи оператора опредеоения типа typeof определить типы следующих переменных и вывести их в консоль let a = 100; let b = '100'; let c = true; let d = undefined;
+- Поставьте соответствующий оператор в выражениях что бы получился соответсвующий результат.
+ В одинаковых выражениях в задании не использовать два одинаковых оператора.
+Каждое выражение вложить в свою переменную
+    5 ? 6 -> true
+    5 ? 6 -> false
+    5 ? 6 -> false
+    5 ? 6 -> false
+    10 ? 10 -> true
+    10 ? 10 -> true
+    10 ? 10 -> false
+    10 ? 10 -> false
+    10 ? 10 -> false
+    123 ? '123' -> false
+    123 ? '123' -> true */
+	
+	// присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false Вывести каждую при помощи console.log , alert, document.write
+
+/*let word='hello';
+let scool='owu';
+let site='com';
+let domen='ua';
+let numb1=1;
+let numb2=10; 
+let numb3=-999;
+let numb4=123;
+let numb5=3.14;
+let numb6=2.7;
+let numb7=16; 
+let isYes=true;
+let isNo=false;
+
+console.log(word, scool, site, domen,numb1, numb2,numb3,numb4,numb5,numb6,numb7, isYes, isNo);
+document.write(word, '<br>', scool,'<br>', site,'<br>', domen, '<br>',numb1,'<br>', numb2, '<br>',numb3, '<br>',numb4, '<br>',numb5, '<br>',numb6,'<br>',numb7, '<br>',isYes,'<br>', isNo);
+alert(word);
+alert(scool);
+alert(site);
+alert(domen);
+alert(numb1);
+alert(numb2);
+alert(numb3);
+alert(numb4);
+alert(numb5);
+alert(numb6);
+alert(numb7);
+alert(isYes);
+alert(isNo);
+*/
+// переопределить каждую переменную из задания 1 дав им произвольные значения Вывести каждую при помощи console.log , alert, document.write
+document.write ('<hr>')
+
+/*let word='It is good';
+let scool='University';
+let site=12;
+let domen=380;
+let numb1=2020;
+let numb2=2021; 
+let numb3=-2022;
+let numb4=2023;
+let numb5=2024.2024;
+let numb6=-45.789;
+let numb7=30; 
+let isYes=null;
+let isNo=false;
+
+console.log(word, scool, site, domen,numb1, numb2,numb3,numb4,numb5,numb6,numb7, isYes, isNo);
+document.write(word, '<br>', scool,'<br>', site,'<br>', domen, '<br>',numb1,'<br>', numb2, '<br>',numb3, '<br>',numb4, '<br>',numb5, '<br>',numb6,'<br>',numb7, '<br>',isYes,'<br>', isNo);
+alert(word);
+alert(scool);
+alert(site);
+alert(domen);
+alert(numb1);
+alert(numb2);
+alert(numb3);
+alert(numb4);
+alert(numb5);
+alert(numb6);
+alert(numb7);
+alert(isYes);
+alert(isNo);  */
+
+// Создать 3 числовых и 3 стринговых константы. Вывести каждую при помощи console.log , alert, document.write
+
+const num1=1;
+const num2=2;
+const num3=3;
+
+const str1='Hi';
+const str2='I am';
+const str3='Roman';
+
+console.log(num1,num2,num3,str1,str2,str3);
+alert(num1);
+alert(num2);
+alert(num3);
+alert(str1);
+alert(str2);
+alert(str3);
+document.write(num1,'<br>', num2,'<br>',num3,'<br>',str1,'<br>',str2,'<br>',str3);
+

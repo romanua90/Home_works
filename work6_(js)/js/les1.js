ba48a334c73872577bb9aed63c4fccfc29757af8
Roman Stashchak <stashchak90@gmail.com>
2020-11-02 00:34:46 +0200

    work 6

--- work6_(js)/js/les1.js
+++ work6_(js)/js/les1.js
@@ -1,37 +1,5 @@
-//document.write("Hello world!");
-//alert("Hello world");
-//a=confirm ("How old are you?")
-//console.log (a);
 
-/*1. task
-
--
-- 
-- ��� ������ 3� ������ prompt() �������� 3 ����� ������� �������� ������ ���. ��� ������� ����� � �������� ������� ������ ����������. ������� ������ ��� ������ console.log , alert, document.write
-- ����� ���������� �� ������� 4 � ���������������� �� � ����� ���������� person.
-- ����� ������ 4 � 5 � ��������� ��� � ��� ���� ������ ����� �����.
-- ��� ������ prompt() �������� 3 �����. �������� �� � ��������� ���� ��� ������ +. ������� �� � �������.
-- ��� ������ prompt() �������� 4 �����. �������� �� � ��������� ���� ��� ������ parseInt. ������� �� ����� ����� ������� ��������� � ���������� result � ������� � ������� ��������
-- ��� ������ prompt()  �������� 3 ����� � ��������� �������. ��� ������ parseFloat �������� �� � ��������������� ����. ������� �� ����� ����� ������� ��������� � ���������� result � ������� � ������� ��������
-- ��� ������ prompt()  �������� 3 ����� � ��������� �������. ��������� �� ��� ������ Math.round ������� �� ����� ����� ������� ��������� � ����������  � ������� � ������� ��������
-- ��� ������ prompt()  �������� 2 ����� �����. �������� �� � �������������� ����. ������ ����� - ��� ����� ������� ����� ��������� � �������. ������ ����� - ��� ����� ������� �������� ��������. ��� ������ Math.pow �������� ������ ����� � ������� ������� �����.
-- ��� ������ ��������� ����������� ���� typeof ���������� ���� ��������� ���������� � ������� �� � ������� let a = 100; let b = '100'; let c = true; let d = undefined;
-- ��������� ��������������� �������� � ���������� ��� �� ��������� �������������� ���������.
- � ���������� ���������� � ������� �� ������������ ��� ���������� ���������.
-������ ��������� ������� � ���� ����������
-    5 ? 6 -> true
-    5 ? 6 -> false
-    5 ? 6 -> false
-    5 ? 6 -> false
-    10 ? 10 -> true
-    10 ? 10 -> true
-    10 ? 10 -> false
-    10 ? 10 -> false
-    10 ? 10 -> false
-    123 ? '123' -> false
-    123 ? '123' -> true */
-	
-	// ��������� ������� �� ��������� �������� ���� ����������: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false ������� ������ ��� ������ console.log , alert, document.write
+// - присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false Вывести каждую при помощи console.log , alert, document.write
 
 /*let word='hello';
 let scool='owu';
@@ -63,8 +31,8 @@ alert(numb7);
 alert(isYes);
 alert(isNo);
 */
-// �������������� ������ ���������� �� ������� 1 ��� �� ������������ �������� ������� ������ ��� ������ console.log , alert, document.write
-document.write ('<hr>')
+// переопределить каждую переменную из задания 1 дав им произвольные значения Вывести каждую при помощи console.log , alert, document.write
+// document.write ('<hr>');
 
 /*let word='It is good';
 let scool='University';
@@ -96,23 +64,121 @@ alert(numb7);
 alert(isYes);
 alert(isNo);  */
 
-// ������� 3 �������� � 3 ���������� ���������. ������� ������ ��� ������ console.log , alert, document.write
+//Создать 3 числовых и 3 стринговых константы. Вывести каждую при помощи console.log , alert, document.write
+//
+// const num1=1;
+// const num2=2;
+// const num3=3;
+//
+// const str1='Hi';
+// const str2='I am';
+// const str3='Roman';
+//
+// console.log(num1,num2,num3,str1,str2,str3);
+// alert(num1);
+// alert(num2);
+// alert(num3);
+// alert(str1);
+// alert(str2);
+// alert(str3);
+// document.write(num1,'<br>', num2,'<br>',num3,'<br>',str1,'<br>',str2,'<br>',str3);
 
-const num1=1;
-const num2=2;
-const num3=3;
 
-const str1='Hi';
-const str2='I am';
-const str3='Roman';
+//_при помощи 3х разных prompt() получить 3 слова которые являются вашими ФИО. Для фамилии имени и отчества создать разные переменные. Вывести каждую при помощи console.log , alert, document.write
 
-console.log(num1,num2,num3,str1,str2,str3);
-alert(num1);
-alert(num2);
-alert(num3);
-alert(str1);
-alert(str2);
-alert(str3);
-document.write(num1,'<br>', num2,'<br>',num3,'<br>',str1,'<br>',str2,'<br>',str3);
+// let Name= prompt ("Please enter your Name");
+// let FirstName= prompt ("Please enter your First Name");
+// let SecondName= prompt ("Please enter your Second Name");
+//
+// console.log (Name, FirstName, SecondName);
+
+// - Взять переменные из задания 4 и сконкатенировать их в одной переменной person.
+
+// let person=Name+" "+FirstName+" "+SecondName;
+//
+// console.log(person);
+
+//- при помощи prompt() получить 3 числа. Привести их к числовому типу при помощи +. вывести их в консоль.
+
+// let Number1= prompt ("Please enter number 1");
+// let Number2= prompt ("Please enter number 2");
+// let Number3= prompt ("Please enter number 3");
+// console.log (+Number1, +Number2, +Number3);
+
+
+// - при помощи prompt() получить 4 числа. Привести их к числовому типу при помощи parseInt. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+
+// let Number1= prompt ("Please enter number 1");
+// let Number2= prompt ("Please enter number 2");
+// let Number3= prompt ("Please enter number 3");
+// let Number4= prompt ("Please enter number 4");
+// let result=parseInt(Number1)+parseInt(Number2)+parseInt(Number3)+parseInt(Number4);
+// console.log (result);
+
+// - при помощи prompt()  получить 3 числа с плавающей точекой. при помощи parseFloat привести их к соответсвующему типу. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+//
+// let Number1= prompt ("Please enter number 1");
+// let Number2= prompt ("Please enter number 2");
+// let Number3= prompt ("Please enter number 3");
+// let result=parseFloat(Number1)+parseFloat(Number2)+parseFloat(Number3);
+// console.log (result);
+
+// - при помощи prompt()  получить 3 числа с плавающей точекой. Округлить их при помощи Math.round Сложить их между собой записав результат в переменную  и вывести в консоль браузера
+
+// let Number1= parseFloat(prompt ("Please enter number 1"));
+// let Number2= parseFloat (prompt("Please enter number 2"));
+// let Number3= parseFloat(prompt ("Please enter number 3"));
+//
+// console.log (Math.round(Number1+Number2+Number3));
+
+
+// - при помощи prompt()  получить 2 целых числа. Привести их к целочисленному типу. Первое число - это число которое будут возводить в степень. Второе число - это число которое является степенью. При помощи Math.pow возвести первое число в степень второго числа.
+
+// let Number1= Math.round(+(prompt ("Please enter number 1")));
+// let Number2= Math.round(+(prompt("Please enter number 2")));
+// result=Math.pow(Number1,Number2);
+// console.log(result);
+
+// - При помощи оператора опредеоения типа typeof определить типы следующих переменных и вывести их в консоль let a = 100; let b = '100'; let c = true; let d = undefined;
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+// console.log("a=",typeof(a), "b=",typeof(b), "c=",typeof(c), "d=",typeof(d));
+
+// - Поставьте соответствующий оператор в выражениях что бы получился соответсвующий результат.
+//
+// console.log (5 != 6 , "true");
+// console.log (5 == 6, "false");
+// console.log (5 > 6, "false");
+// console.log (5 === 6, "false");
+// console.log (10 === 10, "true");
+// console.log (10 == 10, "true");console.log(132 > 100 && 45 < 12 );
+// console.log (10 != 10, "false");
+// console.log (10 > 10, "false");
+// console.log (10 < 10, "false");
+// console.log (123 === '123', "false");
+// console.log (123 == '123',"true");
+
+console.log(132 > 100 && 45 < 12 );
+console.log(34 > 33 && 23 < 90 );
+console.log(99 > 100 && 45 > 12 );
+console.log(132 > 100 || 45 < 12 );
+console.log(111 > 11 || 45 < 111 );
+console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) );
+console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );
+console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) );
+console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));
+console.log(!!'-1');
+console.log(!!-1);
+console.log(!!'0');
+console.log(!!'null');
+console.log(!!'undefined');
+console.log(!!(3/'owu'));
+console.log((111 > 11 || 45 < 111) ||  !!'0');
+console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));
+
+
 
 

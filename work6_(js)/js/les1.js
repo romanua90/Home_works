732c59cbd11423bf72546b8e3e97325123d8f57c
Roman Stashchak <stashchak90@gmail.com>
2020-11-03 17:27:39 +0200

    Update les1.js

--- work6_(js)/js/les1.js
+++ work6_(js)/js/les1.js
@@ -14,7 +14,6 @@ let numb6=2.7;
 let numb7=16; 
 let isYes=true;
 let isNo=false;
-
 console.log(word, scool, site, domen,numb1, numb2,numb3,numb4,numb5,numb6,numb7, isYes, isNo);
 document.write(word, '<br>', scool,'<br>', site,'<br>', domen, '<br>',numb1,'<br>', numb2, '<br>',numb3, '<br>',numb4, '<br>',numb5, '<br>',numb6,'<br>',numb7, '<br>',isYes,'<br>', isNo);
 alert(word);
@@ -47,7 +46,6 @@ let numb6=-45.789;
 let numb7=30; 
 let isYes=null;
 let isNo=false;
-
 console.log(word, scool, site, domen,numb1, numb2,numb3,numb4,numb5,numb6,numb7, isYes, isNo);
 document.write(word, '<br>', scool,'<br>', site,'<br>', domen, '<br>',numb1,'<br>', numb2, '<br>',numb3, '<br>',numb4, '<br>',numb5, '<br>',numb6,'<br>',numb7, '<br>',isYes,'<br>', isNo);
 alert(word);
@@ -161,24 +159,230 @@ alert(isNo);  */
 // console.log (123 === '123', "false");
 // console.log (123 == '123',"true");
 
-console.log(132 > 100 && 45 < 12 );
-console.log(34 > 33 && 23 < 90 );
-console.log(99 > 100 && 45 > 12 );
-console.log(132 > 100 || 45 < 12 );
-console.log(111 > 11 || 45 < 111 );
-console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) );
-console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );
-console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) );
-console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));
-console.log(!!'-1');
-console.log(!!-1);
-console.log(!!'0');
-console.log(!!'null');
-console.log(!!'undefined');
-console.log(!!(3/'owu'));
-console.log((111 > 11 || 45 < 111) ||  !!'0');
-console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));
+/*console.log(132 > 100 && 45 < 12 );  false
+console.log(34 > 33 && 23 < 90 );   true
+console.log(99 > 100 && 45 > 12 ); false
+console.log(132 > 100 || 45 < 12 );  true
+console.log(111 > 11 || 45 < 111 ); true
+console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) ); true
+console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) ); true
+console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) ); false
+console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 )); true
+console.log(!!'-1'); true
+console.log(!!-1); true
+console.log(!!'0'); true
+console.log(!!'null'); true
+console.log(!!'undefined'); true
+console.log(!!(3/'owu')); false
+console.log((111 > 11 || 45 < 111) ||  !!'0'); true
+console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 )); false */
+
+
+/*1.Определите переменные str, num, flag и txt со значениями «Привет», 123, true, «true».
+  При помощи оператора typeof убедитесь, что значения переменных принадлежат типам: string, number и  boolean.*/
+  
+/*let str='Привет';
+  let num= 123;
+  let flag=true;
+  let txt= 'true';
+  console.log (typeof (str),typeof(num), typeof(flag), typeof(txt));*/
+
+/*2. Создайте переменные a1, a2, a3, a4, a5. При помощи математических операторов (сложение, вычитание и т.д.) найдите значения выражений:
+    5 + 3,
+    5 - 3,
+    5 * 3,
+    5 / 3,
+    5 / 3
+    поместив результат каждого выражения в соответствующую переменную. Например, let a1 = 5 + 3.  */
+
+//	let a1= 5 + 3;
+//	let a2= 5 - 3; 
+//	let a3= 5 * 3;
+//	let a4= 5 / 3;
+//	let a5= 5 % 3;
+	
+/*3.  Создайте переменные a6, a7, a8, a9, a10. Поместите в них результат выражений:
+    5 % 3,
+    3 % 5,
+    5 + '3',
+    '5' - 3,
+    75 + 'кг' */
+	
+/*  let a6= 5 % 3;
+	let a7= 3 % 5; 
+	let a8= 5 + '3';
+	let a9= '5' - 3;
+	let a10= 75 + 'кг'; */
+	
+
+/*4. Напишите код, который находит площадь прямоугольника высота 23см. ( переменная height),
+  шириной 10см (переменная width), значение площади должно хранится в числовой переменной s.*/
+  
+/*let height=23;
+  let width=10;
+  let s;
+  
+  console.log (s=height*width); */
+  
+/*5.  Напиши код, который находит объем цилиндра высотой 10м (переменная heightC) и диаметром основания 4м (dC),
+результат поместите в переменную v.  */
+
+/*let heightC=10;
+  let dC=4;
+  let v;
+  console.log  (v=Math.PI*Math.pow(dC/2,2)*heightC); */
+  
+  /*6. У прямоугольного треугольника две стороны n (со значением 3) и m (со значением 4).
+  Найдите гипотенузу k по теореме Пифагора (нужно использовать функцию Math.pow(число, степень) или оператор возведения в степень ** ).*/
+  
+  
+ /* let n=3;
+  let m=4;
+  let k;
+  
+  console.log (k=Math.sqrt((Math.pow(n,2))+Math.pow(m,2)));*/
+  
+   /* 7. Напишите скрипт, который выводит "Hello world", создавши переменную str и выводить спомощью document.write, alert и console.log */
+  
+  /*let str='Hello world';
+  document.write(str);
+  alert(str);
+  console.log(str); */
+  
+  /*8. Вывести в окно браузера при помощи метода alert() следующие данные: Ваше ФИО, возраст, хобби (каждой на новой строки спомощью \n).
+*/
+ // alert("Stashchak\nRoman\nVolodymyrovych");
+ // alert("My\nage\nis\n30");
+ // alert('My\nhobby\nis\nfootball');
+  
+ /*9. Создать 4 переменные с использованием ключевого слова let с именами str1, str2, str3, concatenation.
+  Переменной str1 присвоить фразу ‘Кто ‘, str2 – ‘ты ‘, str3 – ‘такой?’
+  Локальной переменной concatenation присвоить результат конкатенации 3-х строк: str1, str2, str3.
+  Вывести в документ содержимое переменной concatenation спомощью document.write*/
+  
+  /* let str1='Who';
+  let str2='is';
+  let str3='you?';
+  
+  document.write (concatenation=str1+' '+str2+' '+str3);*/
+  
+ /* 10. Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+    
+	*/
+//   let str = "20";
+//	 let a = 5;
+//   document.write(str + a + "<br/>");  // 205
+//   document.write(str - a + "<br/>"); // 15
+//   document.write(str * "2" + "<br/>"); //40
+//   document.write(str / 2 + "<br/>"); //10
+
+/*	11. Какие значения выведет в окно консоли следующий фрагмент кода если его поместить в console.log?*/
+    
+//	console.log(parseInt("3.14"));  // 3
+//  console.log(parseInt("-7.875"));  //-7 
+//  console.log(parseInt("435")); // 435
+//  console.log(parseInt("Вася")); // NaN
+
+/* 12.  С помощью окна ввода, вызываемого методом prompt, пользователь может ввести данные, которые будут использоваться далее, повторите код ниже*/
+    
+	// let str = prompt("Enter something", "ho-ho");
+    // console.log(str);
+/*13. С помощью окна ввода, вызываемого методом prompt, сделать сложение двух чисел, а вывод результата при помощи метода alert*/
+//	let a = prompt("Please enter number 1");
+//	let b = prompt("Please enter number 2");
+//	let sum = parseFloat(a)+parseFloat(b);
+//	alert ("Sum is: "+ sum);
+	
+/*	14. С помощью окна ввода, вызываемого методом prompt, пользователь последовательно выводит имя, фамилию и возраст, а вам не обходимо вывести строку такого вида
+      Доброго вечера Иван Иванов, мои поздравления что вам 32 , а вывод результата при помощи метода alert */
+	  
+//	let Name = prompt("Please enter your name");
+//	let Surname = prompt("Please enter your surname");
+//	let Age = prompt("Please enter your age");
+//	alert (`Good afternoon ${Name} ${Surname}, my wishes, that you are ${Age} old`);
+
+
+/*1. Три різних числа вводяться через prompt().
+За допомоги if else вивести іх в порядку зростання. (відсортувати по зростанню)*/
+
+/*let arr=[];
+		
+for (i=0;i<3; i++){
+	arr[i] = prompt("Введіть число ");
+	}
+	arr.sort();
+	document.write(arr); */
+	
+	
+/*2.
+Все параматры получаем с клавиатуры!!!!
+Имитируем поведение пешехода на перекстке.
+Если светофор зеленый - вывести "иди".
+Если светофор желтый - вывести "подожди".
+Если светофор красный - вывести "стой".
+Если светофор в аварийном режиме вывести "делай что хочешь"!*/	
+/*let signal;
+while (signal!='exit'){
+signal=prompt("Please choice signal of traffic lights (red/yellow/green/other/exit)");
+	switch(signal){
+		case "red":
+		alert("STOP!");
+		break;
+		case "yellow":
+		alert("WAIT!");
+		break;
+		case "green":
+		alert("GO!");
+		break;
+		case "exit":
+		break;
+		delauft:
+		alert("DO WHAT DO YOU WANT!")
+	}
+	}
+*/	
+	
+/*3. Все параметры получаем с клавиатуры!!!!(prompt , confirm)
+Создать переменную isRoadClear которая характеризирует наличие на дороге машин.
+Улучшаем предыдущее задание.
+Если светофор зеленый и машин нет - вывести "иди".
+Если светофор зеленый и машины есть  - вывести подожди пока нарушители проедут".
+Если светофор желтый и машины есть - вывести "жди".
+Если светофор желтый и машин нет - вывести "все рано жди".
+Если светофор красный и машин нет- вывести "стой все рано".
+Если светофор красный - и машины есть вывести "стой и жди".
+Если светофор в аварийном режиме вывести "делай что хочешь"!*/
+let isRoadClear;
+	alert ("Please follow of traffic lights (red/yellow/green/other)");
+
+	if (confirm("Is light GREEN?"){
+	else if ( confirm("Is road clear?")){
+		alert ("GO!");
+	}
+		else 
+		{
+			alert ("Please wait till road is clear!");
+		}
+		
+}
+
+
+	
 
 
 
+	
+	
+	
+	
+	
+	 
+	 
+	 
+	 
+  
+  
+
+  
+  
 

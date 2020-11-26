33b718e35465f2e9f6f54eb296c049fe189a674e
Roman Stashchak <stashchak90@gmail.com>
2020-11-27 00:13:36 +0200

    work 14 in progress

--- work 14_(js_callback)/js/les9.js
+++ work 14_(js_callback)/js/les9.js
@@ -12,3 +12,114 @@
 // Кожна подія має бути з рандомною (не по зростанню) затримкою.
 //
 // #task
+// console.time('ev')
+// setTimeout(function () {
+//     console.log('1');
+// }, 3000)
+// setTimeout(function () {
+//     console.log('2')
+// }, 1500)
+// setTimeout(function () {
+//     console.log('3')
+// }, 1000)
+// console.timeEnd('ev')
+let chance = 0.3;
+function myDay (message){
+       return new Promise((resolve,reject) =>
+    {
+        setTimeout(() => {
+           if( Math.random()>chance){
+               resolve(message);
+           }
+           else{
+               reject('Проспав!');
+           }
+
+        }, 2000)
+    })
+}
+
+function breakFest(breakMsg){
+    console.log('Час гарно поїсти!');
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            if( Math.random()>chance){
+                resolve(breakMsg);
+            }
+            else{
+                reject('А нема що їсти!!');
+            }
+
+        }, 2000)
+    })
+}
+
+function wayToWork(workMsg){
+    console.log('Бігом до роботи , бо не встигнеш');
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            if( Math.random()>chance){
+                resolve(workMsg);
+            }
+            else{
+                reject('Не встиг на маршрутку!!');
+            }
+
+        }, 2000)
+    })
+}
+
+
+function hardWork (hardMsg){
+    console.log('Пора працювати!');
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            if( Math.random()>chance){
+                resolve(hardMsg);
+            }
+            else{
+                reject('На роботі повний завал!');
+            }
+
+        }, 2000)
+    })
+}
+
+function coffeBreak (coffeMsg){
+    console.log('Йду я кавки випю');
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            if( Math.random()>chance){
+                resolve(coffeMsg);
+            }
+            else{
+                reject('Всю каву випили!Напився(((');
+            }
+
+        }, 2000)
+    })
+}
+
+myDay('Будильник')
+.then((result)=>{
+    console.log(result);
+    return breakFest('Мівіна')
+})
+.then((result)=>{
+    console.log(result);
+    return wayToWork('Твоя маршрутка 3А')
+})
+.then((result)=> {
+    console.log(result);
+    return hardWork ('Деадлайн був вчора. Працюй активніше!')
+    })
+.then((result)=>{
+    console.log(result);
+    return coffeBreak ('Пора випити кавки!')
+})
+    .then((result)=>{
+        console.log(result);
+    })
+    .catch(error=>{
+        console.log('error:',error);
+    })

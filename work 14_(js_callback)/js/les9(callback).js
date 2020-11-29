84eea2fd977bcec9943284f7ab396c9d7a78ef42
Roman Stashchak <stashchak90@gmail.com>
2020-11-29 19:43:38 +0200

    work 14 in progress

--- work 14_(js_callback)/js/les9(callback).js
+++ work 14_(js_callback)/js/les9(callback).js
@@ -23,179 +23,200 @@
 //     console.log('3')
 // }, 1000)
 // console.timeEnd('ev')
-let chance = 0.3;
-function myDay (message){
-       return new Promise((resolve,reject) =>
-    {
-        setTimeout(() => {
-           if( Math.random()>chance){
-               resolve(message);
-           }
-           else{
-               reject('Проспав!');
-           }
+let chance = 0.4;
 
-        }, 2000)
-    })
+function myDay(message, cb) {
+    console.log('Мій день:');
+    setTimeout(() => {
+        if (Math.random() <= chance) {
+            cb('Проспав!',null);
+            return;
+        }
+       cb(null, message);
+
+    }, 2000)
 }
 
-function cleanTeeth (cleanMsg){
+function cleanTeeth(cleanMsg, cb) {
     console.log('Бігом до ванни чистити зуби!!!');
-    return new Promise((resolve,reject) =>
-    {
-        setTimeout(() => {
-            if( Math.random()>chance){
-                resolve(cleanMsg);
-            }
-            else{
-                reject('Не можу знайти пасту!');
-            }
 
-        }, 2000)
-    })
+    setTimeout(() => {
+        if (Math.random() <= chance) {
+            cb('Не можу знайти пасту!');
+            return;
+        }
+        cleanMsg='Зуби блистять!';
+        cb(null, cleanMsg);
+
+    }, 2000)
 }
 
 
-function breakFest(breakMsg){
+function breakFest(breakMsg, cb) {
     console.log('Час гарно поїсти!');
-    return new Promise((resolve, reject)=>{
-        setTimeout(() => {
-            if( Math.random()>chance){
-                resolve(breakMsg);
-            }
-            else{
-                reject('А нема що їсти!!');
-            }
+    setTimeout(() => {
+        if (Math.random() <= chance) {
+            cb('А нема що їсти!!');
+            return ;
+        }
+        breakMsg='Мівіна';
+        cb(null, breakMsg);
 
-        }, 2000)
-    })
+    }, 2000)
 }
 
-function wayToWork(workMsg){
+function wayToWork(workMsg, cb) {
     console.log('Бігом до роботи , бо не встигнеш');
-    return new Promise((resolve, reject)=>{
-        setTimeout(() => {
-            if( Math.random()>chance){
-                resolve(workMsg);
-            }
-            else{
-                reject('Не встиг на маршрутку!!');
-            }
+    setTimeout(() => {
+        if (Math.random() <= chance) {
+            cb('Не встиг на маршрутку!!');
+            return;
+        }
+        workMsg='Твоя маршрутка 3А';
+        cb(null, workMsg);
 
-        }, 2000)
-    })
+    }, 2000)
+
 }
 
 
-function hardWork (hardMsg){
+function hardWork(hardMsg, cb) {
     console.log('Пора працювати!');
-    return new Promise((resolve, reject)=>{
-        setTimeout(() => {
-            if( Math.random()>chance){
-                resolve(hardMsg);
-            }
-            else{
-                reject('На роботі повний завал!');
-            }
 
-        }, 2000)
-    })
+    setTimeout(() => {
+        if (Math.random() <= chance) {
+            cb('На роботі повний завал!');
+            return;
+        }
+        hardMsg= 'Дедлайн був вчора. Працюй активніше!';
+        cb(null, hardMsg);
+
+    }, 2000)
 }
 
-function coffeBreak (coffeMsg){
+function coffeBreak(coffeMsg, cb) {
     console.log('Йду я кавки випю');
-    return new Promise((resolve, reject)=>{
-        setTimeout(() => {
-            if( Math.random()>chance){
-                resolve(coffeMsg);
-            }
-            else{
-                reject('Всю каву випили!Напився(((');
-            }
+    setTimeout(() => {
+        if (Math.random() <= chance) {
+            cb('Всю каву випили!Напився(((');
+            return;
+        }
+        coffeMsg='Пора випити кавки!';
+        cb(null, coffeMsg);
 
-        }, 2000)
-    })
+    }, 1500)
+
 }
-function endWork(endMsg){
+
+function endWork(endMsg, cb) {
     console.log('Час іти на хату!');
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            if(Math.random()>chance){
-                resolve(endMsg)
-            }
-            else{
-                reject('Соррян, треба лишитися після роботи')
-            }
-        }, 1500)
-    })
+
+    setTimeout(() => {
+        if (Math.random() <= chance) {
+            cb('Соррян, треба лишитися після роботи');
+            return;
+        }
+        endMsg='Пора закінчувати роботу!';
+        cb(null, endMsg);
+    }, 1500)
+
 }
 
-function eatDinnner(dinnerMsg){
+function eatDinnner(dinnerMsg, cb) {
     console.log('Ти вдома, повечеряй!');
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            if(Math.random()>chance){
-                resolve(dinnerMsg);
-            }
-            else{
-                reject('Вечері немає, хтось забув її приготувати!');
-            }
-        },1500)
-    })
+
+    setTimeout(() => {
+        if (Math.random() <= chance) {
+            cb('Вечері немає, хтось забув її приготувати!');
+            return;
+        }
+        dinnerMsg='Пора закінчувати роботу!';
+        cb(null, dinnerMsg);
+    }, 1500)
+
 }
-function goSleep(sleepMsg){
+
+function goSleep(sleepMsg, cb) {
     console.log('Пора йти спати!');
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            if(Math.random()>chance){
-                resolve(dinnerMsg);
-            }
-            else{
-                reject('Щось не спиться сьогодні!');
-            }
-        },1500)
-    })
+    setTimeout(() => {
+        if (Math.random() <= chance) {
+            cb('Щось не спиться сьогодні!');
+            return;
+        }
+        sleepMsg='Гарних снів';
+        cb(null, sleepMsg);
+
+    }, 1500)
+
 }
 
 
+myDay('Будильник ОК', (err, result) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(result);
+    cleanTeeth(result, (err, result) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log(result);
+        breakFest(result, (err, result) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log(result);
+            wayToWork(result, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log(result);
+                hardWork(result, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log(result);
+                    coffeBreak(result, (err, result) => {
+                        if (err) {
+                            console.log(err);
+                            return;
+                        }
+                        console.log(result);
+                        endWork(result, (err, result) => {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                            console.log(result);
+                            eatDinnner(result, (err, result) => {
+                                if (err) {
+                                    console.log(err);
+                                    return;
+                                }
+                                console.log(result);
+                                goSleep(result, () => {
+                                    if (err) {
+                                        console.log(err);
+                                        return;
+                                    }
+                                    console.log(result);
+                                })
+                            })
+                        })
+                    })
+                })
+
+            })
+        })
+    })
+})
 
 
-myDay('Будильник')
-.then((result)=>{
-    console.log(result);
-    return cleanTeeth('Зуби блистять!')
-})
-    .then((result)=>{
-        console.log(result);
-        return breakFest('Мівіна')
-    })
-.then((result)=>{
-    console.log(result);
-    return wayToWork('Твоя маршрутка 3А')
-})
-.then((result)=> {
-    console.log(result);
-    return hardWork ('Дедлайн був вчора. Працюй активніше!')
-    })
-.then((result)=>{
-    console.log(result);
-    return coffeBreak ('Пора випити кавки!')
-})
-    .then((result)=>{
-        console.log(result);
-        return endWork('Пора закінчувати роботу!')
-    })
-    .then((result)=>{
-        console.log(result);
-        return eatDinnner('Було  дуже смачно!')
-    })
-    .then ((result)=>{
-        console.log(result);
-        return goSleep('Гарних снів')
-    })
-    .then ((result)=>{
-        console.log(result);
-    })
-    .catch(error=>{
-        console.log('error:',error);
-    })
+
+
+
